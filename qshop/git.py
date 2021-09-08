876f9ab9dd28b3eb4aa615ab6378c74705a0df1d
tonyfast <tony.fast@gmail.com>
2021-09-08 12:53:54 -0700

    refactor on git

--- qshop/git.py
+++ qshop/git.py
@@ -1,8 +1,10 @@
+"""models foor using information from a git repository"""
+
 from typing import Any
 
-from pydantic import AnyUrl, Field
+from pydantic import Field
 
-from . import OpenModel, Path, urls
+from . import OpenModel, Path
 
 
 class Git(OpenModel):
@@ -13,19 +15,26 @@ class Git(OpenModel):
     org: str = Field(None, description="organization name")
     branch: str = Field(None, description="active branch name")
     name: str = Field(None, description="repo name")
-    ignore: Any = Field(None, description="a pathspec representation of the gitignore")
+    ignore: Any = Field(
+        None, description="a pathspec representation of the gitignore")
 
-    def __post_init__(self):
+    def set_branch(self):
+        self.branch = self.repo.active_branch.name
+
+    def set_repo(self):
         import git
 
         self.repo = git.Repo()
-        self.branch = self.repo.active_branch.name
-        self.url = self.repo.git.execute(f"git config --get remote.origin.url".split())
+
+    def set_url(self):
+        self.url = self.repo.git.execute(
+            f"git config --get remote.origin.url".split())
         if self.url.endswith(".git"):
             self.url = self.url[:-4]
 
         _, self.org, self.name = self.url.rsplit("/", 2)
 
+    def set_gitignore(self):
         ignore = Path(".gitignore")
         if ignore.exists():
             import pathspec
@@ -33,3 +42,9 @@ class Git(OpenModel):
             self.ignore = pathspec.PathSpec.from_lines(
                 pathspec.GitIgnorePattern, ignore.read_text().splitlines()
             )
+
+    def __post_init__(self):
+        self.set_repo()
+        self.set_branch()
+        self.set_url()
+        self.set_gitignore()
