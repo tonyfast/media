0e43494445c3c32c8314c4031769ba75a701c550
tonyfast <tony.fast@gmail.com>
2021-10-22 10:08:59 -0700

    change nox deploy

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,4 @@ from nox import session
 def build_docs(session):
     session.install("git+https://github.com/tonyfast/qpub@new")
     session.install("nikola", "notebook")
-    session.run("qpub", "blog:metadata")
-    session.run("qpub", "blog:configure")
-    session.run("nikola", "build")
+    session.run("qpub", "blog")
